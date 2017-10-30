b73b4f0a977aef2698dd7846c8d497fd1bdcc688
Rolly Linao <rolleenao07@gmail.com>
2017-10-30 12:41:21 +0800

    Updated Database and Design

--- aesportal/index.php
+++ aesportal/index.php
@@ -1,59 +1,282 @@
 <!DOCTYPE html>
+
+<?php
+    include 'connection.php'; //connect the connection page 
+    if(empty($_SESSION)) // if the session not yet started     
+        session_start(); 
+    if(isset($_SESSION['username']))
+    { // if already login   
+        header("location: home.php"); // send to home page   
+        exit;
+    } 
+?>
+
 <html>
-<head>
-	<title>AES Portal | Log In</title>
-	<link rel="stylesheet" href="views/css/styles.css">
-	<link rel="shortcut icon" href="views/images/head_logo.png" />
-</head>
+    <head>
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="shortcut icon" href="images/head_logo.png" />
+    </head>
+    <title>AES Portal</title>
+    
+    <script language="javascript" type="text/javascript">
+        function fun_val()
+        {
+            var l=document.loginsell.username.value;
+            if(l=="")
+            {
+                alert("Please Enter User name");
+                document.loginsell.username.focus;
+                return false;
+            }
 
-<script language="javascript" type="text/javascript">
-  function fun_val()
-  	{
-  		var l=document.loginsell.username.value;
-  		if(l=="")
-  		{
-  			alert("Please Enter User name");
-  			document.loginsell.username.focus;
-  			return false;
-  		}
+            var p=document.loginsell.password.value;
+            if(p=="")
+            {
+                alert("Please Enter Password");
+                document.loginsell.password.focus;
+                return false;
+            }
+        }
+    </script>
+    
+    <style>
+        /* Full-width input fields */
+        input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
 
-  		var p=document.loginsell.password.value;
-  		if(p=="")
-  		{
-  			alert("Please Enter Password");
-  			document.loginsell.password.focus;
-  			return false;
-  		}
-  	}
-  </script>
+        /* Set a style for all buttons */
+        .login_button {
+            background-color: maroon;
+            color: white;
+            position: relative;
+            top: 65px;
+            padding: 14px 20px;
+            margin: auto;
+            border: none;
+            cursor: pointer;
+            font-family: Arial;
+            font-size: 13px;
+            width: 100%;
+        }
 
-<body>
-	<div id="container">
-		<div id="cover">
-			<span class="logo">
-				<img src="views/images/logo.png" width="150px">
-			</span>
-		</div>
+        .login_button:hover {
+            opacity: 0.8;   
+        }
 
-		<div id="contents">
-			<div id="login">
-				<form action="controller/session.php" method="post">
-					<span class="usertext">Username</span>
-					<input class="user" type="text" name="username" size="50" maxlength="32">
-					<span class="passtext">Password</span>
-					<input class="pass" type="password" name="password" size="50" maxlength="32">
-					<input class="login_button" type="submit" name="gologin" value="Log In" onClick="return fun_val();">
+        .User {
+            position: relative;
+            top: 40px;
+        }
+        
+        .Pass {
+            position: relative;
+            top: 35px;
+        }
+        
+        .User input {
+            text-align: center;
+        }
+        
+        .Pass input {
+            text-align: center;
+        }
+        
+        .login_text {
+            position: absolute;
+            text-align: center;
+            width: 92%;
+            font-family: Arial;
+            font-size: 30px;
+        }
+        
+        /* Extra styles for the cancel button */
+        .cancel_button {
+            width: auto;
+            padding: 10px 18px;
+            background-color: maroon;
+            color: white;
+            margin: auto;
+            border: none;
+            cursor: pointer;
+            font-family: Arial;
+            font-size: 13px;
+        }
 
-					<span class="forgot">
-						<a href="#">forgot your password</a>
+        .cancel_button:hover {
+            opacity: 0.8;
+        }
+
+        /* Center the image and position the close button */
+        .login_container {
+            position: relative;
+            height: 240px;
+            padding: 16px;
+        }
+        
+        .login_container2 {
+            position: relative;
+            top: 20px;
+            padding: 16px;
+        }
+
+        span.password {
+            position: absolute;
+            top: 145px;
+            right: 0px;
+            padding: 15px;
+        }
+        
+        .password a {
+            font-family: Arial;
+            font-size: 13px;
+        }
+        
+        span.noaccount {
+            position: relative;
+            top: 82px;
+            left: 75px;
+            padding-top: 16px;
+            font-family: Arial;
+            font-size: 13px;
+        }
+        
+        .noaccount a {
+            font-family: Arial;
+            font-size: 13px;
+        }
+
+        /* The login_popup (background) */
+        .login_popup {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            padding-top: 60px;
+        }
+
+        /* login_popup Content/Box */
+        .login_popup_content {
+            background-color: #fefefe;
+            margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+            border: 1px solid #888;
+            width: 400px; /* Could be more or less, depending on screen size */
+        }
+
+        /* Add Zoom Animation */
+        .animate {
+            -webkit-animation: animatezoom 0.2s;
+            animation: animatezoom 0.2s
+        }
+
+        @-webkit-keyframes animatezoom {
+            from {-webkit-transform: scale(0)} 
+            to {-webkit-transform: scale(1)}
+        }
+    
+        @keyframes animatezoom {
+            from {transform: scale(0)} 
+            to {transform: scale(1)}
+        }
+
+        /* Change styles for span and cancel button on extra small screens */
+        @media screen and (max-width: 300px) {
+            span.password {
+                display: block;
+                float: none;
+            }
+            .cancel_button {
+                width: 100%;
+            }
+        }
+    </style>
+    
+    <body>
+        <div class="cover">
+            <a href="index.php">
+                <img class="logo" src="images/logo.png" width="100px;">
+            </a>
+        </div>
+        <div class="navbar">
+            <div class="menu">
+                 <div class="dropdown_content">
+                    <button class="dropbutton" >MY ACCOUNT</button>
+                    <div class="dropdown_contents">
+                        <a href="#" onclick="document.getElementById('login_pop').style.display='block'" style="width:auto;">LOG IN</a>
+                    </div>
+                </div>
+                <div class="dropdown_content">
+                    <button class="dropbutton">HELP</button>
+                    <div class="dropdown_contents">
+                        <a href="#">HOW TO USE</a>
+                    </div>
+                </div>
+                <div class="dropdown_content">
+                    <button class="dropbutton">ABOUT</button>
+                    <div class="dropdown_contents">
+                        <a href="#">WEBSITE</a>
+                        <a href="#">DEVELOPMENT</a>
+                        <a href="#">CONTACT US</a>
+                    </div>
+                </div>                
+            </div>
+        </div>
+        
+        <div id="login_pop" class="login_popup">
+            <form class="login_popup_content animate" action="session.php" method="post">
+                <div class="login_container">
+                    <span class="login_text">AES Portal | Log In</span>
+                    <span class="User">
+                        <input type="text" placeholder="Enter Username" name="username" required>
+                    </span>
+                    <span class="Pass">
+                        <input type="password" placeholder="Enter Password" name="password" required>
+                    </span>
+                    <span class="password"><a href="#">forgot your password?</a></span>
+                    
+                    <button class="login_button" type="submit" name="submit" onClick="return fun_val();">Login</button>
+                    
+                    <span class="noaccount">
+						Don't have an account yet?
+						<a href="#"> Ask How</a>
 					</span>
-				</form>
-			</div>
-		</div>
+                    
+                </div>
 
-		<div id="footer">
-			AES Portal &copy 2017
-		</div>
-	</div>
-</body>
+                <div class="login_container2" style="background-color:#f1f1f1">
+                    <button type="button" onclick="document.getElementById('login_pop').style.display='none'" class="cancel_button">Cancel</button>
+                    
+                </div>
+            </form>
+        </div>
+
+        <script>
+            var login_popup = document.getElementById('login_pop');
+
+            window.onclick = function(event) {
+                if (event.target == login_popup) {
+                    login_popup.style.display = "none";
+                }
+            }
+        </script>
+        
+        <div id="content">
+            <div id="content">
+
+            </div>
+            <div id="footer">
+                AES Portal &copy 2017
+            </div>
+	   </div>
+    </body>
 </html>
